96cacd2250a6cea13136a568c012dd1839d23882
cglangs <cdanegallagher@gmail.com>
2021-01-24 15:50:04 -0400

    add threshold slider to change microphone sensitivity

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,12 @@ let socket = io.connect(`${endpoint}`)
 class App extends Component {
 
 
+  state = {
+    threshold_decibels: 65,
+    harkObject: null
+  }
+
   initRecorder(stream, listofBlobs){
-      let _this = this
       let recorder = RecordRTC(stream, {
       type: 'audio',
       sampleRate: 44100,
@@ -27,13 +31,13 @@ class App extends Component {
       return recorder
   }
 
-  componentDidMount(){
+  componentDidMount() {
     let _this = this
     var newAudio = document.createElement('audio');
     newAudio.autoplay = true;
     navigator.mediaDevices.getUserMedia({audio: true }).then(async function(stream) {
-      var recorder = _this.initRecorder(stream)
-        var options = {threshold: -65};
+        var recorder = _this.initRecorder(stream)
+        var options = {threshold: -1 * _this.state.threshold_decibels};//-100 is silence -50 is the default
         var speechEvents = hark(stream, options);
 
         speechEvents.on('speaking', function() {
@@ -53,17 +57,22 @@ class App extends Component {
               ;
           });
         });
+        _this.setState({harkObject: speechEvents})
     });
+  }
 
+  handleSliderChange = (e) => {
+    this.setState({threshold_decibels: e.target.value}, () => this.state.harkObject.setThreshold(-1* this.state.threshold_decibels));
   }
 
 
   render(){
+    //console.log(this.state.harkObject && this.state.harkObject.threshold)
     return (
       <div className="App">
         <header className="App-header">
-
-
+        <input type="range" min="50" max="80" value={this.state.threshold_decibels} onChange={this.handleSliderChange}/>
+        <span>{this.state.threshold_decibels - 50 }</span>
         </header>
       </div>
     );
