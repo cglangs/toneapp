dc6905aa4be97b5cad0291e847a647c9add98132
cglangs <cdanegallagher@gmail.com>
2021-02-15 16:29:31 -0400

    make sorting function

--- order_phrases.py
+++ order_phrases.py
@@ -15,22 +15,32 @@ def get_word_frequencies(phrase_list):
 			word_frequencies[word] += 1
 	return word_frequencies
 
-
-def order_phrases(phrase_list, word_frequencies):
-	return None
+def get_new_words_count_and_min_frequency(word_list, word_frequencies, words_used):
+	new_words = [word for word in word_list if word not in words_used]
+	return len(new_words),  -1 * min([word_frequencies[word] for word in new_words])
 
 def get_data():
 	with open('phrases_edited.tsv') as csvfile:
 		phrase_list=[]
+		sorted_phrase_list = []
+		words_used = set()
 		readCSV = csv.reader(csvfile, delimiter='\t')
 		phrases = list(readCSV)
+		
 		for phrase in phrases:
 			new_phrase = {}
 			new_phrase["full_phrase"]=phrase[0]
 			new_phrase["phrase_no_punctuation"]=regex.sub(r"[{}]+".format(punc), "", phrase[0])
-			new_phrase["word_list"]=list(jieba.cut(new_phrase["phrase_no_punctuation"], cut_all=False, HMM=False))
+			new_phrase["word_list"]=list(jieba.cut(new_phrase["phrase_no_punctuation"], cut_all=False, HMM=False),)
 			phrase_list.append(new_phrase)
 		word_frequencies = get_word_frequencies(phrase_list)
+
+		while len(phrase_list) > 0:
+			next_phrase = sorted(enumerate(phrase_list), key = lambda phrase: get_new_words_count_and_min_frequency(phrase[1]["word_list"], word_frequencies, words_used))[0]
+			sorted_phrase_list.append(next_phrase[1])
+			del phrase_list[next_phrase[0]]
+
+		print(sorted_phrase_list)
 
 
 get_data()
