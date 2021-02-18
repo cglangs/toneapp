505bdb479fd37a553c347ee1212f3793ed89a2b9
cglangs <cdanegallagher@gmail.com>
2021-02-18 11:26:59 -0400

    add tone change for yi

--- order_phrases.py
+++ order_phrases.py
@@ -6,6 +6,9 @@ from pypinyin import pinyin as tones, Style
 
 jieba.set_dictionary("user_dict.txt")
 punc = "！？。，"
+numbers = ["一","二","三","四","五","六","七","八","九","十"]
+prefix_time_words = ["星期"]
+suffix_time_words = ["年","月","号"]
 
 
 def get_word_frequencies(phrase_list):
@@ -21,10 +24,41 @@ def get_new_words_count_and_min_frequency(word_list, word_frequencies, words_use
 	new_words = [word for word in word_list if word not in words_used]
 	return len(new_words),  -1 * min([word_frequencies[word] for word in new_words])
 
-def insert_phrases(sorted_phrase_list):
-	phrase_insert_num = 1
+
+def yiTone(word_before, word_after, tone_after):
+	result_tone = "1"
+	if word_after == None or word_before in numbers or word_after in numbers or word_before in prefix_time_words or word_after in suffix_time_words:
+		result_tone = "1"
+	elif tone_after == "4":
+		result_tone = "2"
+	else:
+		result_tone = "4"
+	return result_tone
 
 
+
+def changeTone(word_list, written_tones):
+	result_tone_list = written_tones.copy()
+	for i in range(len(word_list)):
+		if word_list[i] == "一" and written_tones[i] == "1":
+			word_before = None
+			word_after = None
+			tone_after = None
+			if i>0:
+				word_before = word_list[i-1]
+			if i < len(word_list) -1:
+				word_after = word_list[i+1]
+				tone_after = written_tones[i+1]
+			result_tone_list[i] = yiTone(word_before,word_after,tone_after)
+	return result_tone_list		
+
+
+def spliceTone(wordWithTone):
+	finalChar = wordWithTone[-1]
+	result = finalChar
+	if(not finalChar.isnumeric()):
+		finalChar = "0" 
+	return finalChar
 
 
 def get_data():
@@ -42,8 +76,8 @@ def get_data():
 			new_phrase["word_list"]=list(jieba.cut(new_phrase["phrase_no_punctuation"], cut_all=False, HMM=False))
 			new_phrase["pinyin"]=[pinyin.get(word) for word in new_phrase["word_list"]]
 			toneArray = tones(new_phrase["phrase_no_punctuation"], style=Style.TONE3)
-			new_phrase["written_tones"]= [tone for l in toneArray for tone in l]
-			#get spoken tones
+			new_phrase["written_tones"]= [spliceTone(tone) for l in toneArray for tone in l]
+			new_phrase["spoken_tones"]=changeTone(new_phrase["word_list"],new_phrase["written_tones"])
 			phrase_list.append(new_phrase)
 
 		word_frequencies = get_word_frequencies(phrase_list)
@@ -52,6 +86,8 @@ def get_data():
 			next_phrase = sorted(enumerate(phrase_list), key = lambda phrase: get_new_words_count_and_min_frequency(phrase[1]["word_list"], word_frequencies, words_used))[0]
 			sorted_phrase_list.append(next_phrase[1])
 			del phrase_list[next_phrase[0]]
+
+		#changeTone(sorted_phrase_list)
 
 		print(sorted_phrase_list)
 
