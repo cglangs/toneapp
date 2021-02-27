7ac366a73a4596578fec00ebc4ab7f7ac41200af
cglangs <cdanegallagher@gmail.com>
2021-02-27 12:08:07 -0400

    make Learn.js a functional component

--- client/src/Learn.js
+++ client/src/Learn.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import { useState} from 'react'
 import Characterbycharacter from './Characterbycharacter'
 import Fullsentence from './Fullsentence'
-import {Query} from 'react-apollo';
+import { useQuery } from  'react-apollo';
 import gql from 'graphql-tag';
 import { redirectToLearnComponent} from './utils'
+import { useParams } from "react-router-dom";
 
 
 const GET_PHRASE = gql`
-query getPhrase($deck_id: Int!, $phrase_order: Int!) {
+query getPhrase($deck_id: Int!) {
   getPhrasesInDeck(deck_id: $deck_id) {
     phrase_order
     full_phrase
@@ -20,33 +21,34 @@ query getPhrase($deck_id: Int!, $phrase_order: Int!) {
   }
 }
 `
-class Learn extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      fullSentenceMode: false,
-    }
+
+const UPDATE_PROGRESS = gql`
+mutation updateProgress($deck_id: Int!, $phrase_order: Int!) {
+  updateUserProgress(deck_id: $deck_id, phrase_order: $phrase_order)
+}
+`
+
+const Learn = (props) => {
+
+  const [fullSentenceMode, setSentenceMode] = useState(false);
+
+  const { deckId, phraseOrder } = useParams();
+
+  const { loading, error, data } = useQuery(GET_PHRASE, {
+    variables: {deck_id: parseInt(deckId)}
+  });
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+
+
+  function changeMode(isFullSentenceMode){
+    setSentenceMode(isFullSentenceMode)
   }
 
-  changeMode = (isFullSentenceMode) => {
-    this.setState({fullSentenceMode: isFullSentenceMode})
-  }
-
-  componentDidUpdate = (prevProps, prevState) => {
-    console.log("LEARN UPDATE")
-  }
-
-  getUrlParams = () =>{
-    let params = {}
-    const path = this.props.location.pathname.split("/")
-    const deck_id = parseInt(path[path.length - 2])
-    const phrase_order = parseInt(path[path.length - 1])
-    params.deck_id = deck_id
-    params.phrase_order = phrase_order
-    return params
-  }
-
-  getPhraseDetails = (phrase) => {
+  function getPhraseDetails(phrase){
+    console.log(phrase)
     let phrase_data = {}
     phrase_data["display"] = phrase.full_phrase
     phrase_data["characters"] = phrase.phrase_no_punctuation
@@ -57,37 +59,25 @@ class Learn extends Component {
     return phrase_data
   }
 
-  onClickEvent = (deck_id, phrase_order) => {
-    redirectToLearnComponent(this.props, deck_id, phrase_order)
+  function onClickEvent(deck_id, phrase_order){
+    redirectToLearnComponent(props, deck_id, phrase_order)
   }
 
-  render() {
-    let params = this.getUrlParams()
-    return(
-      <Query query={GET_PHRASE} variables={{deck_id: params.deck_id}}>
-      {({ loading, error, data, refetch }) => {
-        if (loading) return <div>Fetching</div>
-        if (error) return <div>error</div>
-        let phrase = data.getPhrasesInDeck[params.phrase_order - 1]
-        return(
-        <div className="LearnContainer">
-          <div className="Navbar">
-            <p className={!this.state.fullSentenceMode ? "selectedItem": ""} onClick={() => this.changeMode(false)}>Character by Character</p>
-            <p className={this.state.fullSentenceMode ? "selectedItem": ""} onClick={() => this.changeMode(true)}>Full Sentence</p>
-          </div>
-          <div className="toneTrainingInterface">
-          <button disabled={params.phrase_order === 1 } style={{marginRight: "5%"}}  onClick={() => this.onClickEvent(params.deck_id, params.phrase_order - 1)}>{"<"}</button>
-          <div style={{width: "75%"}}>
-          {this.state.fullSentenceMode ? <Fullsentence sentence={this.getPhraseDetails(phrase)}/>: <Characterbycharacter sentence={this.getPhraseDetails(phrase)}/>}
-          </div>
-          <button disabled={params.phrase_order === data.getPhrasesInDeck.length } style={{marginLeft: "5%"}} onClick={() => this.onClickEvent(params.deck_id, params.phrase_order + 1)}>{">"}</button>
-          </div>
-        </div>
-      )
-    }}
-    </Query>
+  return(
+    <div className="LearnContainer">
+      <div className="Navbar">
+        <p className={!fullSentenceMode ? "selectedItem": ""} onClick={() => changeMode(false)}>Character by Character</p>
+        <p className={fullSentenceMode ? "selectedItem": ""} onClick={() => changeMode(true)}>Full Sentence</p>
+      </div>
+      <div className="toneTrainingInterface">
+      <button disabled={phraseOrder === 1 } style={{marginRight: "5%"}}  onClick={() => onClickEvent(deckId,  parseInt(phraseOrder) - 1)}>{"<"}</button>
+      <div style={{width: "75%"}}>
+      {fullSentenceMode ? <Fullsentence sentence={getPhraseDetails(data.getPhrasesInDeck[phraseOrder - 1])}/>: <Characterbycharacter sentence={getPhraseDetails(data.getPhrasesInDeck[phraseOrder - 1])}/>}
+      </div>
+      <button disabled={phraseOrder === data.getPhrasesInDeck.length } style={{marginLeft: "5%"}} onClick={() => onClickEvent(deckId, parseInt(phraseOrder) + 1)}>{">"}</button>
+      </div>
+    </div>
   )
-  }
+    
 }
-
 export default Learn
