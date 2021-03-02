86a7cdc0f90194d0d5c833c2ebffb62e87d674bf
cglangs <cdanegallagher@gmail.com>
2021-03-01 23:05:40 -0400

    add hsk1 words ot phrases file

---
+++ get_hsk_words.py
@@ -0,0 +1,16 @@
+import csv
+
+def get_data():
+	with open('hsk/pseudo_hsk1.tsv') as csvfile:
+		word_dict = {}
+		readCSV = csv.reader(csvfile, delimiter='\t')
+		words = list(readCSV)
+		for word in words:
+			nextDict ={}
+			nextDict["tones"] = word[2]
+			nextDict["pinyin"] = word[3]
+			word_dict[word[0]] = nextDict
+		print(word_dict)
+
+
+get_data()
